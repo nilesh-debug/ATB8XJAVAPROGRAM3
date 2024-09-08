40f8ecd3e8db4a133824554d363dc22abd494dbe
Nilesh <varhadi.nilesh81@gmail.com>
2024-09-08 20:58:34 +0530

    hello vN

--- src/sept/ex_06092024/Lab004.java
+++ src/sept/ex_06092024/Lab004.java
@@ -1,4 +1,10 @@
 package sept.ex_06092024;
 
 public class Lab004 {
+    public static void main(String[] nilesh) {
+
+        System.out.println("Hello world");
+//        System.out.println("Hello ");
+
+    }
 }
